c4e03ff7c3140b09e074f9afcd258e742f6fcf96
Lev Leontev <lev.leontev@jetbrains.com>
2024-11-22 18:07:10 +0100

    updates

--- src/main/java/com/example/myapplication/MainActivity.kt
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.myapplication
 
 import android.app.Activity
 import android.os.Bundle
+import android.provider.MediaStore
 
 class MainActivity : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    // Should not be red because of android_min_sdk in the project view
+    MediaStore.getExternalVolumeNames(this)
   }
 }
